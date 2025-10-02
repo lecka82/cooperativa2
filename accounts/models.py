a93f4c69941d22b3503bf778c92938e3075a3c08
lecka82 <135836067+lecka82@users.noreply.github.com>
2025-10-02 08:22:50 -0300

    Troca ImageField por CloudinaryField e ajusta leitura do .env

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -9,7 +10,7 @@ class Profile(models.Model):
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    foto = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    foto = CloudinaryField('foto', blank=True, null=True)
     cargo = models.CharField(max_length=20, choices=CARGOS, default="operacional")
 
     def __str__(self):
